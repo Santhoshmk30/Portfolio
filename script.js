a92fbfacb55cb08a0b031bff45b2f272a1a5e44c
M.K.SANTHOSH <121882993+Santhoshmk30@users.noreply.github.com>
2024-07-16 10:41:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-var tablinks= document.getElementsByClassName("tab-links");
-var tabcontents= document.getElementsByClassName("tab-contents");
+var tablinks= document.getElementsByClassName("tab_links");
+var tabcontents= document.getElementsByClassName("tab_contents");
 function opentab(tabname){
-    for(tablink of tablinks){
-        tablink.classList.remove("active-link");
+    for(tab_links of tablinks){
+        tab_links.classList.remove("active-link");
     }
-    for(tabcontent of tabcontents){
-        tabcontent.classList.remove("active-tab");
+    for(tab_contents of tabcontents){
+        tab_contents.classList.remove("active-tab");
     }
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
